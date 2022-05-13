2ca73ea15ae02bc4f9d66dae855969691456125f
C4CTu <thelokojulian@gmail.com>
2022-05-12 21:21:47 -0300

    123

--- Ejercicio Clase 5/Ejercicio/Ejercicio/Program.cs
+++ Ejercicio Clase 5/Ejercicio/Ejercicio/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Porfavor ingrese su nombre");
+
+string nombre = Console.ReadLine();
+
+Console.WriteLine("¡Hola " + nombre + "!");
+
+Console.WriteLine("¿Desea continuar? Ingrese 'S' para una respuesta afirmativa o 'N' para una negativa");
+
+string continuar = Console.ReadLine();
+
+if (continuar.ToUpper() == "S")
+ {
+        Console.WriteLine("1232");
+ }
+else if (continuar.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamete");
+}
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
+    
+Console.ReadKey();
