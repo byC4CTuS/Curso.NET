963e3b917d1bd64a1deb57665bdeb3a438afa941
C4CTu <thelokojulian@gmail.com>
2022-05-17 22:20:51 -0300

    terminado

--- Ejercicio Clase 5/Ejercicio/Ejercicio/Program.cs
+++ Ejercicio Clase 5/Ejercicio/Ejercicio/Program.cs
@@ -1,5 +1,9 @@
 ﻿Console.Clear();
 
+string continuar = "S";
+
+while (continuar.ToUpper() == "S")
+{ 
 Console.WriteLine("Porfavor ingrese su nombre");
 
 string nombre = Console.ReadLine();
@@ -8,12 +12,11 @@ Console.WriteLine("¡Hola " + nombre + "!");
 
 Console.WriteLine("¿Desea continuar? Ingrese 'S' para una respuesta afirmativa o 'N' para una negativa");
 
-string continuar = Console.ReadLine();
+continuar = Console.ReadLine();
 
-if (continuar.ToUpper() == "S")
- {
-        Console.WriteLine("1232");
- }
+    if (continuar.ToUpper() == "S")
+    { 
+    }
 else if (continuar.ToUpper() == "N")
 {
     Console.WriteLine("Programa finalizado correctamete");
@@ -22,5 +25,6 @@ else
 {
     Console.WriteLine("Opcion no valida");
 }
-    
-Console.ReadKey();
+
+}
+    Console.ReadKey();
