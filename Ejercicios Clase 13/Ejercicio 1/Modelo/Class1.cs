b586390c3b0aba64bd544b1ed5a1a2ca869baf8c
C4CTu <thelokojulian@gmail.com>
2022-06-17 19:33:57 -0300

    clase 13

---
+++ Ejercicios Clase 13/Ejercicio 1/Modelo/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Modelo
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe20 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+        public decimal Total()
+        {
+            decimal total = (BilletesDe10 * 10) + (BilletesDe20 * 20) + (BilletesDe50 * 50) + (BilletesDe100 * 100) + (BilletesDe200 * 200) + (BilletesDe500 * 500) + (BilletesDe1000 * 1000);
+            return total;
+        }
+        public Billetera Combinar(Billetera billetera2)
+        {
+            Billetera billetera3 = new Billetera()
+            {
+                BilletesDe10 = BilletesDe10 + billetera2.BilletesDe10,
+                BilletesDe20 = BilletesDe20 + billetera2.BilletesDe20,
+                BilletesDe50 = BilletesDe50 + billetera2.BilletesDe50,
+                BilletesDe100 = BilletesDe100 + billetera2.BilletesDe100,
+                BilletesDe200 = BilletesDe200 + billetera2.BilletesDe200,
+                BilletesDe500 = BilletesDe500 + billetera2.BilletesDe500,
+                BilletesDe1000 = BilletesDe1000 + billetera2.BilletesDe1000
+            };
+
+            BilletesDe10 = BilletesDe20 = BilletesDe50 = BilletesDe100 = BilletesDe200 = BilletesDe500 = BilletesDe1000 = 0;
+            
+            billetera2.BilletesDe10 = billetera2.BilletesDe20 = billetera2.BilletesDe50 = billetera2.BilletesDe100 = billetera2.BilletesDe200 = billetera2.BilletesDe500 = billetera2.BilletesDe1000 = 0;
+            return billetera3;
+        }
+    }
+}
---
+++ Ejercicios Clase 13/Ejercicio 1/Program.cs
@@ -0,0 +1,60 @@
+﻿/* 
+Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+BilletesDe10
+BilletesDe20
+BilletesDe50
+BilletesDe100
+BilletesDe200
+BilletesDe500
+BilletesDe1000
+
+Agregar un método que se llame Total del tipo decimal, y que devuelva el Importe Total en la billetera teniendo en cuenta la cantidad de billetes que se tenga de cada tipo.
+
+Agregar un metodo que se llame Combinar, que reciba como parámetro otra billetera y que devuelva una nueva Billetera con la suma o combinacion del dinero de ambas billeteras.  
+Una vez combinadas las 2 billeteras (y creada la tercera), las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+*/
+
+using Ejercicio_1.Modelo;
+
+var billetera1 = new Billetera()
+{
+    BilletesDe10 = 2,
+    BilletesDe20 = 3,
+    BilletesDe50 = 10,
+    BilletesDe100 = 7,
+    BilletesDe200 = 5,
+    BilletesDe500 = 3,
+    BilletesDe1000 = 5
+};
+
+
+Console.WriteLine(billetera1.Total());
+
+
+var billetera2 = new Billetera()
+{
+    BilletesDe10 = 4,
+    BilletesDe20 = 8,
+    BilletesDe50 = 1,
+    BilletesDe100 = 2,
+    BilletesDe200 = 3,
+    BilletesDe500 = 5,
+    BilletesDe1000 = 10
+};
+
+Console.WriteLine(billetera2.Total());
+
+
+var billetera3 = new Billetera();
+
+billetera3 = billetera1.Combinar(billetera2);
+
+
+Console.WriteLine(billetera1.Total());
+Console.WriteLine(billetera2.Total());
+
+Console.WriteLine("BILLETERA 3");
+Console.WriteLine(billetera3.Total());
+
+
